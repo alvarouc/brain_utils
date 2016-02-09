427fd06e30983bb874d192ebae422b9297fddf87
Alvaro Ulloa <alvarouc@gmail.com>
2016-02-09 16:05:06 -0700

    fixed plot_source to work with indeces

--- plot.py
+++ plot.py
@@ -11,33 +11,42 @@ def plot_brain(data, mask):
 
 
 def plot_source(source, template, mask, th=3, vmin=-5, vmax=5):
-
+    '''
+    INPUT:
+    source - 1d vector with weight values
+    template - 3d array with background image
+    mask - 1d vector with indices that map the source to 3d
+    th - threshold to plot on top of background
+    '''
+    
     # find maximum position
-    temp = np.zeros(mask.shape)
-    temp[mask] = source
-    x, y, z = np.where(np.abs(temp) == np.abs(temp).max())
+    shape = template.shape
+
+    def unravel(x): return np.unravel_index(x, shape)
+
+    temp = np.zeros(shape)
+    temp[unravel(mask)] = source
+    x, y, z = unravel(np.abs(temp).argmax())
     sx = np.squeeze(temp[x, :, :])
     sy = np.squeeze(temp[:, y, :])
     sz = np.squeeze(temp[:, :, z])
 
     # draw template on maximum position
-    temp = np.zeros(mask.shape)
-    temp[mask] = template
     plt.subplot(1, 3, 3)
-    plt.imshow(np.squeeze(temp[:, :, z]), cmap=cm.gray)
+    plt.imshow(np.squeeze(template[:, :, z]), cmap=cm.gray)
     masked_data = np.ma.masked_where(np.abs(sz) < th, sz)
     plt.imshow(masked_data, cmap=cm.jet, interpolation='none',
                vmin=vmin, vmax=vmax)
 
     plt.subplot(1, 3, 1)
-    plt.imshow(np.flipud(np.squeeze(temp[:, y, :]).T), cmap=cm.gray)
+    plt.imshow(np.flipud(np.squeeze(template[:, y, :]).T), cmap=cm.gray)
     sy = np.flipud(sy.T)
     masked_data = np.ma.masked_where(np.abs(sy) < th, sy)
     plt.imshow(masked_data, cmap=cm.jet, interpolation='none',
                vmin=vmin, vmax=vmax)
 
     plt.subplot(1, 3, 2)
-    plt.imshow(np.flipud(np.squeeze(temp[x, :, :]).T), cmap=cm.gray)
+    plt.imshow(np.flipud(np.squeeze(template[x, :, :]).T), cmap=cm.gray)
     sx = np.flipud(sx.T)
     masked_data = np.ma.masked_where(np.abs(sx) < th, sx)
     plt.imshow(masked_data, cmap=cm.jet,
